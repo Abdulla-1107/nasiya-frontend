d366633a962b722f94204c390d2867067e7432f2
Abdulla <abdusattorovabdulla60@gmail.com>
2025-07-03 11:55:05 +0500

    add partner seller payment crud

---
+++ src/features/parter/service/usePartner.ts
@@ -0,0 +1,36 @@
+import { partner } from "@/shared/keys";
+import { api } from "@/shared/lib/axios";
+import { useQuery, useMutation, useQueryClient, keepPreviousData } from "@tanstack/react-query";
+
+interface IParams {
+    page?: string,
+    limit?: string,
+    search?: string,
+    role: string,
+    isActive?: string,
+    sortBy?: string,
+    sortOrder?: "asc" | "desc",
+    debtOnly?: string
+}
+
+export const usePartner = () => {
+  const queryClient = useQueryClient();
+
+  const getPartners = (params:IParams) =>
+    useQuery({
+      queryKey: [partner, params],
+      queryFn: () => api.get("partners", {params}).then((res) => res.data),
+      placeholderData: keepPreviousData,
+      staleTime: 1000 * 60 * 5, // re-fetch vaqti
+      gcTime: 1000 * 60 * 10 // cache vaqti
+    });
+  
+  const createPartner = useMutation({
+    mutationFn: (body:any)=> api.post("partners", body).then(res => res.data),
+    onSuccess:()=>{
+      queryClient.invalidateQueries({queryKey: [partner]})
+    }
+  })
+
+  return { getPartners, createPartner };
+};
